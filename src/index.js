795858aaec4058e3e099d45e91f8ac711c25188a
Journie Ma-Johnson <journie.johnson@fastly.com>
2022-12-08 17:24:11 -0800

    middleware fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { Router } from "@fastly/expressly";
 
 const router = new Router();
 
+//use middleware to set a header
+router.use((req, res) => {
+  res.set("x-powered-by", "expressly");
+});
+
 //GET 200 response
 router.get('/', (req, res) => {
   res.sendStatus(200); // "OK"
@@ -14,11 +19,6 @@ router.post("/submit", async (req, res) => {
   let body = await req.text();
   res.send(`You posted: "${body}"`)
 })
-
-//use middleware to set a header
-router.use((req, res) => {
-  res.setHeader("x-powered-by", "expressly");
-});
 
 // 404/405 response for everything else
 
